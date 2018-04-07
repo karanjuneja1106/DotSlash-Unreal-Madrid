4d84dca9eaf71efdd60840d7fa6ae7c49119fe0d
Karan Juneja <karan.juneja1106@gmail.com>
2018-04-07 19:38:02 +0530

    Image saving - firebase added

--- Android/CampusBuddy/app/src/main/java/com/buddy/campus/campusbuddy/PostAdActivity.java
+++ Android/CampusBuddy/app/src/main/java/com/buddy/campus/campusbuddy/PostAdActivity.java
@@ -2,29 +2,47 @@ package com.buddy.campus.campusbuddy;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.json.JSONObject;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class PostAdActivity extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    FirebaseAuth auth;
     DatabaseReference myRef = database.getReference("Advertisements");
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://campusbuddy-d4179.appspot.com");
+
     EditText mTitleText,mDescText;
     Button mButton,mImgButton;
     ImageView imageView;
@@ -32,11 +50,12 @@ public class PostAdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_ad);
+        auth = FirebaseAuth.getInstance();
         mButton = findViewById(R.id.upload);
         mTitleText = findViewById(R.id.product_title);
         mDescText = findViewById(R.id.product_desc);
         mImgButton=findViewById(R.id.photo_sel);
-        imageView=findViewById(R.id.viewImage);
+        imageView=findViewById(R.id.imageView);
 
         mImgButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,26 +76,41 @@ public class PostAdActivity extends AppCompatActivity {
                 }
                 str = mDescText.getText().toString();
                 M.put("DESC",str);
+                if(str.length()==0){
+                    Toast.makeText(getApplicationContext(),"Description can't be empty",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String fileName = UUID.randomUUID().toString()+".jpeg";
+                M.put("IMG", fileName);
+                M.put("USER",auth.getCurrentUser().getEmail());
                 JSONObject json = new JSONObject(M);
-                if(str.length()>0){
-                    myRef.push().child("ad").setValue(json.toString());
-                    Toast.makeText(getApplicationContext(),"Posted successfully",Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-                else{
-                        Toast.makeText(getApplicationContext(),"Description can't be empty",Toast.LENGTH_SHORT).show();
-                }
+                BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] data = baos.toByteArray();
+                StorageReference mountainsRef = storageRef.child(fileName);
+                UploadTask uploadTask = mountainsRef.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        Toast.makeText(getApplicationContext(),"File upload failed,Please try again",Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    }
+                });
+                myRef.push().child("ad").setValue(json.toString());
+                Toast.makeText(getApplicationContext(),"Posted successfully",Toast.LENGTH_SHORT).show();
+                finish();
+
             }
         });
     }
     private void selectImage() {
-
-
-
         final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
-
-
-
         AlertDialog.Builder builder = new AlertDialog.Builder(PostAdActivity.this);
         builder.setTitle("Add Photo!");
         builder.setItems(options, new DialogInterface.OnClickListener() {
@@ -84,10 +118,8 @@ public class PostAdActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int item) {
                 if (options[item].equals("Take Photo"))
                 {
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
-                    startActivityForResult(intent, 1);
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1);
                 }
                 else if (options[item].equals("Choose from Gallery"))
                 {
@@ -101,4 +133,26 @@ public class PostAdActivity extends AppCompatActivity {
         });
         builder.show();
     }
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(photo);
+            } else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                imageView.setImageBitmap(thumbnail);
+            }
+        }
+    }
+
 }
